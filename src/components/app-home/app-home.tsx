b004f888be700c5d24be3857edb19e916ce9346a
ivaylo-mobile-dev <ivayloslav@hotmail.com>
2018-06-11 19:36:49 -0600

    initial commit

--- src/components/app-home/app-home.tsx
+++ src/components/app-home/app-home.tsx
@@ -11,17 +11,18 @@ export class AppHome {
     return [
       <ion-header>
         <ion-toolbar color='primary'>
-          <ion-title>Ionic PWA Toolkit</ion-title>
+          <ion-title>Ivaylo Slavov's PWA Resume</ion-title>
         </ion-toolbar>
       </ion-header>,
 
       <ion-content padding>
         <main>
           <p>
-            Welcome to the Ionic PWA Toolkit.
-            You can use this starter to build entire PWAs all with
-            web components using Stencil and ionic/core! Check out the readme for everything that comes in this starter out of the box and
-            Check out our docs on <a href='https://stenciljs.com'>stenciljs.com</a> to get started.
+            This is my Resume Progressive Web App implementation using the
+            <a href='https://blog.ionicframework.com/announcing-the-ionic-pwa-toolkit-beta/'> Ionic PWA Toolkit</a>.
+            I used this starter to build the entire PWAs all with web components using Stencil and ionic/core!
+            Check out the readme for everything that comes in this starter out of the box and
+            Check out the Stencil docs on <a href='https://stenciljs.com'>stenciljs.com</a> to get started.
           </p>
 
           <ion-button href='/profile/ionic'>
